1c3148991fa5169a38b151a1fcc71298935fcd88
soumik-rickysaha <soumik.rickysaha@gmail.com>
2018-05-05 18:24:36 +0530

    Practise concepts-First Commit

---
+++ PractiseConcepts/src/main/java/java_Selenium/DebugPractise.java
@@ -0,0 +1,25 @@
+package java_Selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class DebugPractise 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+    	String str1="test";
+    	String str2="test";
+    	int i=1;
+    	int j=2;
+    	
+    	System.out.println(str1);
+    	System.out.println(str2);
+    	System.out.println(i);
+    	System.out.println(j);
+    	if (i==j) {
+			System.out.println("same");
+		}
+    }
+}
